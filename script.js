da509439b61bb4e9d4788bee0fa20e57eb521350
Reese Netro <reesie.netro@gmail.com>
2016-08-12 14:52:31 -0700

    Changing to watercolor theme

---
+++ script.js
@@ -0,0 +1,25 @@
+myID = document.getElementById("myID");
+
+var myScrollFunc = function() {
+  var y = window.scrollY;
+  if (y > 0 && y <= 50) {
+    myID.className = "sun pt1"
+  } 
+  else if (y>50 && y<150) {
+    myID.className = "sun pt2"
+  }  
+  else if (y>=150 && y<=300) {
+    myID.className = "sun pt3"
+  }  
+  else if (y>300 && y<500) {
+    myID.className = "sun pt4"
+  }  
+  else if (y>500) {
+    myID.className = "sun pt5"
+  }  
+  else {
+    myID.className = "sun none"
+  }
+};
+
+window.addEventListener("scroll", myScrollFunc);
