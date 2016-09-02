092f1212a66d2e5ea4f02c69f39c8b2e17d97664
Reese Netro <reesie.netro@gmail.com>
2016-09-01 17:00:49 -0700

    Commenting out some extra script

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ myID = document.getElementById("myID");
 
 var myScrollFunc = function() {
   var y = window.scrollY;
-  if (y > 10 && y <= 50) {
+  if (y > 10 && y <= 150) {
     myID.className = "sun pt1"
   } 
-  else if (y>50 && y<150) {
+  else if (y>150) {
     myID.className = "sun pt2"
   }  
+/**
   else if (y>=150 && y<=300) {
     myID.className = "sun pt3"
   }  
@@ -17,6 +18,7 @@ var myScrollFunc = function() {
   else if (y>500) {
     myID.className = "sun pt5"
   }  
+  **/
   else {
     myID.className = "sun none"
   }
